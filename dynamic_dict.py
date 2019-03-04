002f4e8e093b17213af204db7ec8f3a82f912026
pthapa3 <pthapa3@hotmail.com>
2019-03-04 17:25:52 +0000

    Edited

--- dynamic_dict.py
+++ dynamic_dict.py
@@ -34,7 +34,7 @@ def update(dict1, dict2, key):
                 else:
                     dict1[k] = dict1.get(k)+dict2.get(k)
 
-return {key:dict1}
+    return {key:dict1}
 
 
 
@@ -67,11 +67,8 @@ def create_current_dict(json_list, cmd_args):
         logging.debug("Leaf values as arrays of flat dictionaries created\n")
         
         for i in range(len(cmd_args.keys)-1):
-            
             current_dict = {json_dict.get(cmd_args.keys[-2-i]): current_dict}
     
-    
-        
         keys = list(current_dict.keys())
         
         for key in keys:
@@ -87,7 +84,8 @@ def create_current_dict(json_list, cmd_args):
                     nested_dict[key] = nested_dict.get(key) + get_dict.get(key)
 
 
-                        return nested_dict
+    return nested_dict
+
 
 def check_dup_args(key_args):
     if (len(key_args)==0):
@@ -105,43 +103,42 @@ def check_dup_args(key_args):
 def run():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                         level=logging.DEBUG)
-        
-                        # If input is not given, it will throw an error and terminate the program
-                        while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-                            json_input = sys.stdin.read()
-                            if json_input:
-                                json_obj = json.loads(json_input)
+    # If input is not given, it will throw an error and terminate the program
+    while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+        json_input = sys.stdin.read()
+        if json_input:
+                json_obj = json.loads(json_input)
+                
+                logging.debug("Json Array input accepted. Ready to create nested dictionary\n")
+                
+                arg_parser = argparse.ArgumentParser()
+                arg_parser.add_argument('keys', nargs = '*')
+                
+                cmdline_args = arg_parser.parse_args()
+                logging.debug("Key arguments parsed. Validating key arguments\n")
+                
+                if (check_dup_args(cmdline_args.keys) == False):
+                    sys.stderr.write("\nExiting Program\n")
+                    exit(0)
+                        
+                else:
+                    try:
+                        current_dictionaries  = create_current_dict(json_obj, cmdline_args)
+                        array_input = 'Json array input:\n'+ json.dumps(json_obj, indent=4)+'\n'
+                        dictionary_output = '\nNested dictionary:\n'+(json.dumps(current_dictionaries, indent=4))
+                        sys.stdout.write(array_input)
+                        sys.stdout.write(dictionary_output)
+                            
+                    except Exception as e:
+                        sys.stderr.write("Error")
+                        logging.error("Exception occurred", exc_info=True)
                                 
-                                logging.debug("Json Array input accepted. Ready to create nested dictionary\n")
-                                
-                                arg_parser = argparse.ArgumentParser()
-                                arg_parser.add_argument('keys', nargs = '*')
-                                
-                                cmdline_args = arg_parser.parse_args()
-                                logging.debug("Key arguments parsed. Validating key arguments\n")
-                                
-                                if (check_dup_args(cmdline_args.keys) == False):
-                                    sys.stderr.write("\nExiting Program\n")
-                                    exit(0)
-                                        
-                                        else:
-                                            try:
-                                                current_dictionaries  = create_current_dict(json_obj, cmdline_args)
-                                                array_input = 'Json array input:\n'+ json.dumps(json_obj, indent=4)+'\n'
-                                                dictionary_output = '\nNested dictionary:\n'+(json.dumps(current_dictionaries, indent=4))
-                                                sys.stdout.write(array_input)
-                                                sys.stdout.write(dictionary_output)
-                                                    
-                                                    except Exception as e:
-                                                        sys.stderr.write("Error")
-                                                        logging.error("Exception occurred", exc_info=True)
-                                                            
-                                                            else:
-                                                                sys.stderr.write('\n\n******** Program Ended... ********\n')
-                                                                exit(0)
-                                                            else:
-                                                                sys.stderr.write('\nError: ********** Please give a valid json file as an input and run it as:\
-                                                                                 \n cat Yourfilename.json | python nest.py key1 [optional keys......] \n**********\n')
+        else:
+            sys.stderr.write('\n\n******** Program Ended... ********\n')
+            exit(0)
+    else:
+        sys.stderr.write('\nError: ********** Please give a valid json file as an input and run it as:\
+                         \n cat Yourfilename.json | python nest.py key1 [optional keys......] \n**********\n')
 
 
 
